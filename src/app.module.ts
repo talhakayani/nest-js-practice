4242398825a32893e437db42699c7a7bf098a92d
talhakayani12 <talha.kayani@invozone.com>
2023-02-06 15:31:10 +0500

    Removed MongoDB URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@storagechain.3ffeqey.mongodb.net/test',
-    ),
-  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
